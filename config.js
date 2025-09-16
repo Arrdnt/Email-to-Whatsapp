a25beb1adefe1aa7a01f6bd2a0c5df42fced785e
Super Milezar <153404908+supermilezar@users.noreply.github.com>
2025-09-16 13:26:49 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+{
+  "admins": ["6212345678"],
+  "groups": {
+    "Array1": {
+      "senders": [
+        "inserturallowedsenders@mail"
+      ],
+      "target": "62812345678"
+    },
+    "Array2": {
+      "senders": ["info.umum@kampus.ac.id"],
+      "target": "62857xxxx@c.us"
+    }
+  },
+  "default_target": "62812xxxx@c.us"
+}
